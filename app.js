bfc55eb92c39ad6ca79a12befb63f96fd31653da
unknown <8268363@gmail.com>
2023-07-31 01:51:49 +0300

    node

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ const app = express()
 
 const PORT = process.env.PORT ?? 3000
 
+app.get('/', (_req, res) => {
+    res.status(200).end()
+})
+
 app.listen(PORT)
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from time import sleep
 import requests
 
 def start():
-    x = requests.get('https://bushkabybot-b050.onrender.com')
-    print(x.status_code)
+    x = requests.get('https://bushkabybot-refresher.onrender.com')
+    y = requests.get('https://bushkabybot-b050.onrender.com')
+    print(x.status_code, y.status_code)
     sleep(120)
     start()
 
