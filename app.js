6768841e97a786eadb38f00a6a0fb870ec11f1aa
unknown <8268363@gmail.com>
2023-07-31 02:12:12 +0300

    python

--- app.js
+++ app.js
@@ -21,17 +21,17 @@ app.listen(PORT, () => {
             return;
         }
         console.log(`stdout: ${stdout}`);
-    })
-    
-    child_process.exec("python main.py", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
+
+        child_process.exec("python main.py", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        })
     })
 })
