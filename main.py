b04a6a65e7ea31de4da84fc430d7053354501dd2
unknown <8268363@gmail.com>
2023-07-31 00:15:23 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from time import sleep
+import requests
+
+def start():
+    x = requests.get('https://bushkabybot-b050.onrender.com')
+    print(x.status_code)
+    sleep(30)
+    start()
+
+start()
