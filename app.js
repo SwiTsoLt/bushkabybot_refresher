e92abcf0db9487e7b0193ccef791ace8cec0618c
Stanislav Smirnov <8268363@gmail.com>
2023-07-31 13:03:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
 const child_process = require('child_process');
 const express = require('express');
+const axios = require('axios') 
 
 const app = express()
 
 const PORT = process.env.PORT ?? 3000
 
+
+function start() {
+  setInterval(()=>{
+axios.get('https://bushkabybot-b050.onrender.com')
+  }, 60000) 
+} 
+
 app.get('/', (_req, res) => {
     res.status(200).end()
 })
 
-app.listen(PORT, () => {
-    child_process.exec("python -m pip install --upgrade pip", () => {
-        child_process.exec("python -m pip install requests", () => {
-            child_process.exec("python main.py", (_, msg) => {
-                console.log(msg);
-            })
-        })
-    })
-})
+app.listen(PORT, start)
